66cb2e3a35e5bbcaf05b7c8de8653c1e52d93850
RAGUNATH-R-30 <62740632+RAGUNATH-R-30@users.noreply.github.com>
2024-02-29 12:19:49 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,26 +1,20 @@
 async function covid_data(){
     var api = await fetch("https://data.covid19india.org/v4/min/data.min.json")
     var api_data = await api.json();
-    var confirmed = api_data.TN.districts.Chennai.total.confirmed.toLocaleString();
-    var deceased = api_data.TN.districts.Chennai.total.deceased.toLocaleString();
-    var recovered =api_data.TN.districts.Chennai.total.recovered.toLocaleString();
-    var tested = api_data.TN.districts.Chennai.total.tested.toLocaleString();
-    var vaccinated1 = api_data.TN.districts.Chennai.total.vaccinated1.toLocaleString();
-    var vaccinated2 = api_data.TN.districts.Chennai.total.vaccinated2.toLocaleString();
+    var data_path = api_data.TN.districts.Chennai.total
     var div = document.createElement("div")
     div.innerHTML= `<div class="card bg-light mb-3" style="max-width: 18rem;">
     <div class="card-header"><h5>Chennai-Covid-Data</h5></div>
     <div class="card-body">
-      <p class="card-text">Confirmed: ${confirmed}.</p>
-      <p class="card-text">Deceased: ${deceased}.</p>
-      <p class="card-text">Recovered: ${recovered}.</p>
-      <p class="card-text">Tested: ${tested}.</p>
-      <p class="card-text">Vaccinated1: ${vaccinated1}.</p>
-      <p class="card-text">Vaccinated2: ${vaccinated2}.</p>
+      <p class="card-text">Confirmed: ${data_path.confirmed.toLocaleString()}.</p>
+      <p class="card-text">Deceased: ${data_path.deceased.toLocaleString()}.</p>
+      <p class="card-text">Recovered: ${data_path.recovered.toLocaleString()}.</p>
+      <p class="card-text">Tested: ${data_path.tested.toLocaleString()}.</p>
+      <p class="card-text">Vaccinated1: ${data_path.vaccinated1.toLocaleString()}.</p>
+      <p class="card-text">Vaccinated2: ${data_path.vaccinated2.toLocaleString()}.</p>
     </div>
   </div>`
   document.body.append(div)
 }
 
 covid_data();
-
